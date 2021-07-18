1c022c16aac6467db99bb0728fee7c5e0223e3ee
Daniyal Farooque <danf7861@gmail.com>
2021-07-18 15:19:24 +0530

    Updated PORT

--- app.js
+++ app.js
@@ -173,6 +173,6 @@ app.post("/login", function(req, res){
 
 });
 
-app.listen(3000, function() {
-  console.log("Server started on port 3000.");
+app.listen(process.env.PORT || 3000, function() {
+  console.log("Server started on port.");
 });
